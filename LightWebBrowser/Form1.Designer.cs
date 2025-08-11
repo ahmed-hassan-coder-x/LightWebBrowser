d74bc804925ad9f8e6625cd93e56f728bd5ff727
AʜᴍᴇᴅCᴏᴅᴇʀX <ahmedxhassan36@gmail.com>
2025-08-11 12:04:58 +0300

    Explained how to work with any C# design focusing on `Form1.Designer.cs` structure and usage

--- LightWebBrowser/Form1.Designer.cs
+++ LightWebBrowser/Form1.Designer.cs
@@ -46,16 +46,16 @@ namespace LightWebBrowser
                                     | System.Windows.Forms.AnchorStyles.Right)));
             this.txtUrl.Location = new System.Drawing.Point(12, 12);
             this.txtUrl.Name = "txtUrl";
-            this.txtUrl.Size = new System.Drawing.Size(600, 22);
+            this.txtUrl.Size = new System.Drawing.Size(750, 22);
             this.txtUrl.TabIndex = 0;
             this.txtUrl.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txtUrl_KeyDown);
             // 
             // btnGo
             // 
             this.btnGo.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.btnGo.Location = new System.Drawing.Point(618, 11);
+            this.btnGo.Location = new System.Drawing.Point(768, 10);
             this.btnGo.Name = "btnGo";
-            this.btnGo.Size = new System.Drawing.Size(50, 24);
+            this.btnGo.Size = new System.Drawing.Size(50, 26);
             this.btnGo.TabIndex = 1;
             this.btnGo.Text = "Go";
             this.btnGo.UseVisualStyleBackColor = true;
@@ -63,61 +63,62 @@ namespace LightWebBrowser
             // 
             // btnBack
             // 
-            this.btnBack.Location = new System.Drawing.Point(12, 40);
+            this.btnBack.Location = new System.Drawing.Point(12, 45);
             this.btnBack.Name = "btnBack";
-            this.btnBack.Size = new System.Drawing.Size(75, 27);
+            this.btnBack.Size = new System.Drawing.Size(65, 30);
             this.btnBack.TabIndex = 2;
-            this.btnBack.Text = "Back";
+            this.btnBack.Text = "←";
             this.btnBack.UseVisualStyleBackColor = true;
             this.btnBack.Click += new System.EventHandler(this.btnBack_Click);
             // 
             // btnForward
             // 
-            this.btnForward.Location = new System.Drawing.Point(93, 40);
+            this.btnForward.Location = new System.Drawing.Point(83, 45);
             this.btnForward.Name = "btnForward";
-            this.btnForward.Size = new System.Drawing.Size(75, 27);
+            this.btnForward.Size = new System.Drawing.Size(65, 30);
             this.btnForward.TabIndex = 3;
-            this.btnForward.Text = "Forward";
+            this.btnForward.Text = "→";
             this.btnForward.UseVisualStyleBackColor = true;
             this.btnForward.Click += new System.EventHandler(this.btnForward_Click);
             // 
             // btnRefresh
             // 
-            this.btnRefresh.Location = new System.Drawing.Point(174, 40);
+            this.btnRefresh.Location = new System.Drawing.Point(154, 45);
             this.btnRefresh.Name = "btnRefresh";
-            this.btnRefresh.Size = new System.Drawing.Size(75, 27);
+            this.btnRefresh.Size = new System.Drawing.Size(75, 30);
             this.btnRefresh.TabIndex = 4;
-            this.btnRefresh.Text = "Refresh";
+            this.btnRefresh.Text = "⟳";
             this.btnRefresh.UseVisualStyleBackColor = true;
             this.btnRefresh.Click += new System.EventHandler(this.btnRefresh_Click);
             // 
             // btnStop
             // 
-            this.btnStop.Location = new System.Drawing.Point(255, 40);
+            this.btnStop.Location = new System.Drawing.Point(235, 45);
             this.btnStop.Name = "btnStop";
-            this.btnStop.Size = new System.Drawing.Size(75, 27);
+            this.btnStop.Size = new System.Drawing.Size(75, 30);
             this.btnStop.TabIndex = 5;
-            this.btnStop.Text = "Stop";
+            this.btnStop.Text = "✖";
             this.btnStop.UseVisualStyleBackColor = true;
             this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
             // 
             // btnHome
             // 
-            this.btnHome.Location = new System.Drawing.Point(336, 40);
+            this.btnHome.Location = new System.Drawing.Point(316, 45);
             this.btnHome.Name = "btnHome";
-            this.btnHome.Size = new System.Drawing.Size(75, 27);
+            this.btnHome.Size = new System.Drawing.Size(75, 30);
             this.btnHome.TabIndex = 6;
-            this.btnHome.Text = "Home";
+            this.btnHome.Text = "🏠";
             this.btnHome.UseVisualStyleBackColor = true;
             this.btnHome.Click += new System.EventHandler(this.btnHome_Click);
             // 
             // btnSettings
             // 
-            this.btnSettings.Location = new System.Drawing.Point(604, 40);
+            this.btnSettings.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnSettings.Location = new System.Drawing.Point(824, 45);
             this.btnSettings.Name = "btnSettings";
-            this.btnSettings.Size = new System.Drawing.Size(64, 27);
+            this.btnSettings.Size = new System.Drawing.Size(75, 30);
             this.btnSettings.TabIndex = 9;
-            this.btnSettings.Text = "Settings";
+            this.btnSettings.Text = "⚙";
             this.btnSettings.UseVisualStyleBackColor = true;
             this.btnSettings.Click += new System.EventHandler(this.btnSettings_Click);
             // 
@@ -125,9 +126,9 @@ namespace LightWebBrowser
             // 
             this.progressBar1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
                                     | System.Windows.Forms.AnchorStyles.Right)));
-            this.progressBar1.Location = new System.Drawing.Point(12, 73);
+            this.progressBar1.Location = new System.Drawing.Point(12, 80);
             this.progressBar1.Name = "progressBar1";
-            this.progressBar1.Size = new System.Drawing.Size(656, 10);
+            this.progressBar1.Size = new System.Drawing.Size(887, 8);
             this.progressBar1.TabIndex = 10;
             // 
             // webView21
@@ -135,15 +136,15 @@ namespace LightWebBrowser
             this.webView21.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
                                     | System.Windows.Forms.AnchorStyles.Left)
                                     | System.Windows.Forms.AnchorStyles.Right)));
-            this.webView21.Location = new System.Drawing.Point(12, 89);
+            this.webView21.Location = new System.Drawing.Point(12, 94);
             this.webView21.MinimumSize = new System.Drawing.Size(20, 20);
             this.webView21.Name = "webView21";
-            this.webView21.Size = new System.Drawing.Size(656, 349);
+            this.webView21.Size = new System.Drawing.Size(887, 450);
             this.webView21.TabIndex = 12;
             // 
             // Form1
             // 
-            this.ClientSize = new System.Drawing.Size(986, 450);
+            this.ClientSize = new System.Drawing.Size(911, 556);
             this.Controls.Add(this.webView21);
             this.Controls.Add(this.progressBar1);
             this.Controls.Add(this.btnSettings);
